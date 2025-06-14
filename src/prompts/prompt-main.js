310860ba8fecefbc46fc8e238a8668e718b8b64b
RicToni <ricardotonholli@gmail.com>
2025-06-13 23:11:21 -0300

    create prompt configure

---
+++ src/prompts/prompt-main.js
@@ -0,0 +1,11 @@
+import { message } from "prompt";
+
+const mainPrompt = [
+    {
+        name: "select",
+        description: "Escolha a ferramenta (1 - QRCODE ou (2 - PASSWORD",
+        pattern: /^[1-2]+$/,
+        message: "Escolha apenas entre 1 e 2",
+        require: true,
+    }
+]
